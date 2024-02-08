edbaf979d184360b6c19faab2f5662eb5e857b31
Md Ghufran Alam <93045939+Ghufran2002@users.noreply.github.com>
2024-02-08 19:59:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "ghufranalam855@gmail.com"
+var accountPassword = "12345"
+accountCity = "muzaffarpur"
+
+
+// accountId =2 // not allowed
+
+/*
+prefer not to use var 
+beacause of issue in block scope and functional scope 
+*/
+
+accountEmail = "ghufran123@gmail.com"
+accountPassword = 2193233
+accountCity = "Bengaluru"
+
+console.table([accountEmail, accountId, accountCity, accountPassword])
+
