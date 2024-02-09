c701f107913da02457de8e86a1daa69732b36251
Md Ghufran Alam <93045939+Ghufran2002@users.noreply.github.com>
2024-02-09 19:03:00 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+
+// ************************operations **********************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1= "hello"
+let str2 = " ghufran"
+
+let str3 = str1+str2
+console.log(str3);
+
