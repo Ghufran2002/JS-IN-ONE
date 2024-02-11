3f52eeb77741a61df49a150fb5e8048b50d86092
Md Ghufran Alam <93045939+Ghufran2002@users.noreply.github.com>
2024-02-11 10:04:03 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,7 +20,7 @@ function addTwoNumbers(number1 , number2){
 
 const result = addTwoNumbers(3,5)
 
-console.log("Result", result);
+//console.log("Result", result);
 
 
 function loginUserMessage (username = "arman"){
@@ -31,4 +31,30 @@ function loginUserMessage (username = "arman"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("farhan"))
+//console.log(loginUserMessage("farhan"))
+
+function calculateCarPrice (val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCarPrice(200, 400, 500 , 2000))
+
+const user = {
+    username: "ghufran",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user);
+
+handleObject({
+    username : "sam",
+    price : 499
+})
+
+const myNewArray = [200,400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
