c00a77a6f9c3d93b74f03336fdd7a8cbec4e8842
Md Ghufran Alam <93045939+Ghufran2002@users.noreply.github.com>
2024-02-11 00:47:19 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,34 @@
+function sayMyName (){
+    console.log("A")
+    console.log("M")
+    console.log("A")
+    console.log("N")
+}
+
+//sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1 , number2){
+    //let result = number1 + number2
+    //return result
+    return number1 + number2
+
+}
+
+const result = addTwoNumbers(3,5)
+
+console.log("Result", result);
+
+
+function loginUserMessage (username = "arman"){
+    if(!username){
+        console.log("Please enter a username");
+        return 
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("farhan"))
